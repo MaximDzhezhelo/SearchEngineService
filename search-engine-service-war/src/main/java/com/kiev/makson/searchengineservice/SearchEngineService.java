52b7054db6d72539b55db90bec5846997204de19
MaximDzhezhelo <mdzhezhelo@gmail.com>
2017-12-30 11:28:52 +0200

    created application.yaml for application, RunnerTest;

---
+++ search-engine-service-war/src/main/java/com/kiev/makson/searchengineservice/SearchEngineService.java
@@ -0,0 +1,11 @@
+package com.kiev.makson.searchengineservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchEngineService {
+
+    public static void main(String[] args) { SpringApplication.run(SearchEngineService.class); }
+
+}
---
+++ search-engine-service-war/src/test/java/com/kiev/makson/searchengine/test/RunnerTest.java
@@ -0,0 +1,29 @@
+package com.kiev.makson.searchengine.test;
+
+import com.kiev.makson.searchengineservice.SearchEngineService;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import javax.annotation.PostConstruct;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = SearchEngineService.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("h2")
+public abstract class RunnerTest extends AbstractTransactionalJUnit4SpringContextTests{
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    protected MockMvc mockMvc;
+
+    @PostConstruct
+    public void init(){mockMvc  = MockMvcBuilders.webAppContextSetup(webApplicationContext).build(); }
+
+}
